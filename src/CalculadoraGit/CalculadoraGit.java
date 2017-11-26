19b008e5066f2a34a7e1442b3daf69be4a147975
maria <mgcmaria@gmail.com>
2017-11-26 09:55:56 +0100

    Version 1

--- src/CalculadoraGit/CalculadoraGit.java
+++ src/CalculadoraGit/CalculadoraGit.java
@@ -1,5 +1,20 @@
 package CalculadoraGit;
 
 public class CalculadoraGit {
+	
+	private int num1;
+	private int num2;
+	
+	public CalculadoraGit (int a, int b)
+	{
+		num1=a;
+		num2=b;
+	}
+	
+	public int suma ()
+	{
+		int resul=num1+num2;
+		return resul;
+	}
 
 }
