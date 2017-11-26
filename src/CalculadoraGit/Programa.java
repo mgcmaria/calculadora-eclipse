78dd5c1101c488b7f6ba818d17c4fd9b615c3409
maria <mgcmaria@gmail.com>
2017-11-26 21:15:45 +0100

    Version 2.0

--- src/CalculadoraGit/Programa.java
+++ src/CalculadoraGit/Programa.java
@@ -1,14 +1,35 @@
 package CalculadoraGit;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+
 public class Programa {
 
 	public static void main(String[] args) {
 		
-		CalculadoraGit calcular = new CalculadoraGit(16,8);
+		Scanner teclado=new Scanner(System.in);
+		
+		int n1,n2;
+		
+		System.out.println("Introduce el primer numero: ");
+		n1=teclado.nextInt();
+		System.out.println("Introduce el segundo numero: ");
+		n2=teclado.nextInt();
+		System.out.println("----------------------");
+		teclado.close();
+		
+		CalculadoraGit calcular1 = new CalculadoraGit(n1,n2);
+		CalculadoraGit calcular = new CalculadoraGit(20,10);
 		
 		System.out.println("El resultado de sumar es: " + calcular.suma());
 		System.out.println("El resultado de restar es: " + calcular.resta());
-
+		System.out.println("----------------------");
+		System.out.println("El resultado de sumar es: " + calcular1.suma());
+		System.out.println("El resultado de restar es: " + calcular1.resta());
+		System.out.println("----------------------");
+		
+		LocalDate hoy = LocalDate.now();
+		System.out.println("Hoy es: "+hoy);
+		System.out.println("Hoy es con otro formato: "+hoy.getDayOfMonth()+"/"+hoy.getMonthValue()+"/"+hoy.getYear());
 	}
-
 }
