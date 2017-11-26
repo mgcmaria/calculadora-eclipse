7653a126c7f9e7ba20d740c00afc24a35de1b508
Elias <unmail@gmail.com>
2017-11-26 22:16:40 +0100

    prueba de subida

--- src/CalculadoraGit/CalculadoraGit.java
+++ src/CalculadoraGit/CalculadoraGit.java
@@ -1,5 +1,16 @@
 package CalculadoraGit;
 
 public class CalculadoraGit {
-
+	
+	
+	private static int num1;
+	  private  static int num2;
+	    public void calculadoraGit (int a, int b){
+	        num1=a;
+	        num2=b;
+	    }
+	public static int divide(int a, int b){
+        int resul=num1/num2;
+        return resul;
+        }
 }
