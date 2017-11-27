6bd8186864c70776a113d1131c464447eb9fdcdc
Elias <unmail@gmail.com>
2017-11-27 19:10:44 +0100

    pruebas de sumas y restas

--- src/CalculadoraGit/Programa.java
+++ src/CalculadoraGit/Programa.java
@@ -21,11 +21,11 @@ public class Programa {
 		CalculadoraGit calcular1 = new CalculadoraGit(n1,n2);
 		CalculadoraGit calcular = new CalculadoraGit(20,10);
 		
-		System.out.println("El resultado de sumar es: " + calcular.suma(4, 6));
-		System.out.println("El resultado de restar es: " + calcular.resta(7, 2));
+		System.out.println("El resultado de sumar es: " + calcular.suma(n1, n2));
+		System.out.println("El resultado de restar es: " + calcular.resta(n1, n2));
 		System.out.println("----------------------");
-		System.out.println("El resultado de sumar es: " + calcular1.suma(78, 4));
-		System.out.println("El resultado de restar es: " + calcular1.resta(2, 98));
+		System.out.println("El resultado de sumar es: " + calcular1.suma(n1, n2));
+		System.out.println("El resultado de restar es: " + calcular1.resta(n1, n2));
 		System.out.println("----------------------");
 		
 		LocalDate hoy = LocalDate.now();
