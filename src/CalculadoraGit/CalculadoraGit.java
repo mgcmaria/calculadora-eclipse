4d9682fdc28439774594407c024678fb0e70debe
rodrigo <34012568+RodrigoRramil@users.noreply.github.com>
2017-11-28 13:12:01 +0100

    CalculadoraGit 5.0 suma, resta, multiplicación, división y suma de los cuadrados.

---
+++ src/CalculadoraGit/CalculadoraGit.java
@@ -0,0 +1,65 @@
+package CalculadoraGit;
+
+public class CalculadoraGit {
+	
+	private static int num1;
+	private static int num2;
+	
+	public CalculadoraGit (int a, int b)
+	{
+		setNum1(a);
+		setNum2(b);
+	}
+	//método suma
+	public static int suma (int n1, int n2)
+	{
+		int resul=getNum1()+getNum2();
+		return resul;
+	}
+	
+	//método resta
+	public static int resta(int n1, int n2)
+	{
+		int resul=getNum1()-getNum2();
+		return resul;
+	}
+	
+	//método multiplicar
+	public static int multiplica (int n1, int n2)
+	{
+		int resul=getNum1()*getNum2();
+		return resul;
+	}
+	
+	//método dividir
+	public static int divide (int n1, int n2)
+	{
+		int resul=getNum1()/getNum2();
+		return resul;
+	}
+	
+	public static int sumaCuadrados (int n1, int n2)
+	{
+		int resul=getNum1()*getNum1()+getNum2()*getNum2();
+		return resul;
+	}	
+	
+	
+	
+	// metodos get y set
+	public static int getNum1() {
+		return num1;
+	}
+
+	public void setNum1(int num1) {
+		this.num1 = num1;
+	}
+
+	public static int getNum2() {
+		return num2;
+	}
+
+	public void setNum2(int num2) {
+		this.num2 = num2;
+	}
+}
---
+++ src/CalculadoraGit/Programa.java
@@ -0,0 +1,33 @@
+package CalculadoraGit;
+
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		Scanner teclado=new Scanner(System.in);
+		
+		int n1,n2;
+		
+		System.out.println("Introduce el primer numero: ");
+		n1=teclado.nextInt();
+		System.out.println("Introduce el segundo numero: ");
+		n2=teclado.nextInt();
+		System.out.println("----------------------");
+		teclado.close();
+		
+		CalculadoraGit calcular1 = new CalculadoraGit(n1,n2);	
+
+		System.out.println(+n1+" + "+n2+" = " + CalculadoraGit.suma(n1, n2));
+		System.out.println(+n1+" - "+n2+" = " + CalculadoraGit.resta(n1, n2));
+		System.out.println(n1+" * "+n2+" = " + CalculadoraGit.multiplica(n1, n2));
+		System.out.println(n1+" / "+n2+" = " + CalculadoraGit.divide(n1, n2));
+		System.out.println(n1+"^2 + "+n2+"^2 = " + CalculadoraGit.sumaCuadrados(n1, n2));
+		System.out.println("----------------------");		
+		LocalDate hoy = LocalDate.now();
+		System.out.println("Hoy es: "+hoy);
+		System.out.println("Hoy es con otro formato: "+hoy.getDayOfMonth()+"/"+hoy.getMonthValue()+"/"+hoy.getYear());
+	}
+}
